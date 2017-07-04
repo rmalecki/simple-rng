0e0a8802d398a55df00a32c7cf98366c9756f642
Rouven Malecki <mail@rouvenmalecki.net>
2017-07-04 14:48:50 +0200

    DRY up

--- lib/simplerng.ex
+++ lib/simplerng.ex
@@ -17,52 +17,24 @@ defmodule SimpleRNG do
   @doc """
   Returns integer between min (inclusive) and max (exclusive)
   """
-  def get_int(seed, min, max) when max <= min, do: {seed, min}
-  def get_int(seed, min, max) do
-    {seed_new, x} = get_uint(seed)
-    {seed_new, min + rem(x, max - min)}
-  end
+  def get_int(_, min, max) when max <= min, do: min
+  def get_int(i, min, max), do: min + rem(i, max - min)
 
   @doc """
   Returns uniform random sample 0.0 > x < 1.0
   """
-  def get_uniform(seed) do
-    {seed_new, x} = get_uint(seed)
-    {seed_new, (x + 1.0) / (:math.pow(2, 32) + 2)}
-  end
+  def get_uniform(x), do: (x + 1.0) / (:math.pow(2, 32) + 2)
 
   @doc """
   Returns normal (Gaussian) random sample with mean 0 and standard deviation 1
   """
-  def get_normal(seed) do
+  def get_normal(i1, i2) do
     # Box-Muller algorithm
-    {seed2, u1} = get_uniform(seed)
-    {seed3, u2} = get_uniform(seed2)
+    u1 = get_uniform(i1)
+    u2 = get_uniform(i2)
     r = :math.sqrt(-2.0 * :math.log(u1))
     theta = 2.0 * :math.pi * u2
-    {seed3, r * :math.sin(theta)}
-  end
-
-  # Swaps elements a and b in a list
-  defp swap(xs, a, b) when a === b, do: xs
-  defp swap(xs, a, b) when b < a, do: swap(xs, b, a)
-  defp swap(xs, a, b) do
-    s1 = Enum.slice(xs, 0, a)
-    [x | s2] = Enum.slice(xs, a, b - a)
-    [y | s3] = Enum.slice(xs, b, Enum.count(xs))
-    s1 ++ [y | s2] ++ [x | s3]
-  end
-
-  # Swap element at a with element at random position before a
-  defp shuffle(xs, seed, a) when a === 0, do: {seed, xs}
-  defp shuffle(xs, seed, a) do
-    {new_seed, b} = get_int(seed, 0, a)
-    xs |> swap(a - 1, b) |> shuffle(new_seed, a - 1)
-  end
-
-  # Fisher-Yates (Knuth) shuffle a list
-  def shuffle(seed, xs) do
-    xs |> shuffle(seed, Enum.count(xs))
+    r * :math.sin(theta)
   end
 
 end
@@ -90,6 +62,10 @@ defmodule SimpleRNG.Server do
     GenServer.start_link(__MODULE__, @default_seed, name: __MODULE__)
   end
 
+  def start(), do: GenServer.start(__MODULE__, @default_seed)
+
+  def stop(pid), do: GenServer.stop(pid)
+
   @doc """
   Set a new seed for the RNG
   """
@@ -102,51 +78,51 @@ defmodule SimpleRNG.Server do
   @doc """
   Returns random unsigned 64bit int
   """
-  def get_uint do
-    GenServer.call __MODULE__, :get_uint
+  def next do
+    GenServer.call __MODULE__, :next
   end
 
-  def get_int(min, max) do
-    GenServer.call __MODULE__, {:get_uint, min, max}
+  def next(pid) do
+    GenServer.call pid, :next
   end
 
-  def get_uniform do
-    GenServer.call __MODULE__, :get_uniform
-  end
-
-  def get_normal do
-    GenServer.call __MODULE__, :get_normal
-  end
-
+  # Fisher-Yates (Knuth) shuffle a list
   def shuffle(xs) do
     GenServer.call __MODULE__, {:shuffle, xs}
+  end
+
+  def shuffle(xs, pid) do
+    GenServer.call pid, {:shuffle, xs}
   end
 
   ######
   # GenServer implementation
 
-  def handle_call(:get_uint, _from, seed) do
+  # Swaps elements a and b in a list
+  defp swap(xs, a, b) when a === b, do: xs
+  defp swap(xs, a, b) when b < a, do: swap(xs, b, a)
+  defp swap(xs, a, b) do
+    s1 = Enum.slice(xs, 0, a)
+    [x | s2] = Enum.slice(xs, a, b - a)
+    [y | s3] = Enum.slice(xs, b, Enum.count(xs))
+    s1 ++ [y | s2] ++ [x | s3]
+  end
+
+  # Swap element at a with element at random position before a
+  defp shuffle(xs, seed, a) when a === 0, do: {seed, xs}
+  defp shuffle(xs, seed, a) do
+    {new_seed, n} = SimpleRNG.get_uint(seed)
+    b = SimpleRNG.get_int(n, 0, a)
+    xs |> swap(a - 1, b) |> shuffle(new_seed, a - 1)
+  end
+
+  def handle_call(:next, _from, seed) do
     {new_seed, n} = SimpleRNG.get_uint(seed)
     {:reply, n, new_seed}
   end
 
-  def handle_call({:get_int, min, max}, _from, seed) do
-    {new_seed, n} = SimpleRNG.get_int(seed, min, max)
-    {:reply, n, new_seed}
-  end
-
-  def handle_call(:get_uniform, _from, seed) do
-    {new_seed, n} = SimpleRNG.get_uniform(seed)
-    {:reply, n, new_seed}
-  end
-
-  def handle_call(:get_normal, _from, seed) do
-    {new_seed, n} = SimpleRNG.get_normal(seed)
-    {:reply, n, new_seed}
-  end
-
   def handle_call({:shuffle, xs}, _from, seed) do
-    {new_seed, shuffled} = SimpleRNG.shuffle(seed, xs)
+    {new_seed, shuffled} = xs |> shuffle(seed, Enum.count(xs))
     {:reply, shuffled, new_seed}
   end
 
